cfedae0406814787b3a21c79c7b7d310cbe88b0a
Urmila <urmilatentu26@gmail.com>
2025-01-08 01:57:54 +0530

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+
 import cors from "cors";
+
+import path from "path";
+
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import { app, httpServer } from "./lib/socket.js";
-import path from "path";
-import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -27,18 +29,17 @@ app.use("/api/messages", messageRoutes);
 
 // Start the server
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // Production build handling
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
 httpServer.listen(PORT, () => {
   console.log("Server is running on port:" + PORT);
