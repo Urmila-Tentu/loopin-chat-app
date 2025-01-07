d28308962f1215a528770705a60e41bda88e9ccd
Urmila <urmilatentu26@gmail.com>
2025-01-08 01:00:46 +0530

    project completed

---
+++ backend/src/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import {connectDB} from './lib/db.js';
+import cookieParser from 'cookie-parser';
+import { app , httpServer } from './lib/socket.js';
+
+dotenv.config();
+
+// Middleware
+app.use(express.json({limit: '10mb'}));
+app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+
+
+// Start the server
+const PORT = process.env.PORT;
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist" , "index.html"));
+    });
+}
+
+httpServer.listen(PORT, () => {
+    console.log('Server is running on port:' + PORT);
+    connectDB();
+});
