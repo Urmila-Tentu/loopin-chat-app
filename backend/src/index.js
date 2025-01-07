11141e6256afb3777e200a950694de233e59d00c
Urmila <urmilatentu26@gmail.com>
2025-01-08 01:37:16 +0530

    fixed es6 error of path module

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,40 +1,46 @@
-import express from 'express';
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import {connectDB} from './lib/db.js';
-import cookieParser from 'cookie-parser';
-import { app , httpServer } from './lib/socket.js';
-import path from 'path';
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import { app, httpServer } from "./lib/socket.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 // Middleware
-app.use(express.json({limit: '10mb'}));
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes);
-
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // Start the server
 const PORT = process.env.PORT;
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist" , "index.html"));
-    });
+// Production build handling
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
 }
 
 httpServer.listen(PORT, () => {
-    console.log('Server is running on port:' + PORT);
-    connectDB();
+  console.log("Server is running on port:" + PORT);
+  connectDB();
 });
